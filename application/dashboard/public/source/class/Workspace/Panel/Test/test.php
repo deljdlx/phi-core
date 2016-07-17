cd66dd853ab0b4edd027fd31df7370e9d22fc20e
biniou <biniou@vmdebian00.lan>
2016-07-17 23:06:00 +0200

    js tree+divers

--- application/dashboard/public/source/class/Workspace/Panel/Test/test.php
+++ application/dashboard/public/source/class/Workspace/Panel/Test/test.php
@@ -1,4 +1,27 @@
+
+
 <div class="col-lg-12">
+
+<?php include(__DIR__.'/fragment/dialog.php'); ?>
+</div>
+
+
+
+
+
+
+
+<div class="col-lg-12">
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -15,7 +38,7 @@
 				<p>Ce texte a pour autre avantage d'utiliser des mots de longueur variable, essayant de simuler une occupation normale. La méthode simpliste consistant à copier-coller un court texte plusieurs fois (« ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ceci est un faux-texte ») a l'inconvénient de ne pas permettre une juste appréciation typographique du résultat final.</p>
 				<p>Il circule des centaines de versions différentes du Lorem ipsum, mais ce texte aurait originellement été tiré de l'ouvrage de Cicéron, De Finibus Bonorum et Malorum (Liber Primus, 32), texte populaire à cette époque, dont l'une des premières phrases est : « Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... » (« Il n'existe personne qui aime la souffrance pour elle-même, ni qui la recherche ni qui la veuille pour ce qu'elle est... »).</p>
 			</div>
-			
+
 		</div>
 	</div>
 
