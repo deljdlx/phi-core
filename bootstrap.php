e793634b65d19640da1db5b8f3dc541ca314fcca
Jean luc <you@example.com>
2017-01-08 14:01:16 +0100

    synchro

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,17 @@
 <?php
 
 if(!defined('PHI_INITIALIZED')) {
-    include(__DIR__.'/source/class/Autoloader.php');
-    $helpers=glob(__DIR__.'/source/helper/*.php');
 
+    //include(__DIR__.'/source/class/Autoloader.php');
+	include(__DIR__.'/vendor/autoload.php');
+
+    $helpers=glob(__DIR__.'/source/helper/*.php');
 
     foreach ($helpers as $helper) {
         include($helper);
     }
-    registerNamespace('Phi', __DIR__.'/source/class');
+
+    //registerNamespace('Phi', __DIR__.'/source/class');
 
 
     //@TODO autoload modules
