6b18649be72acd7a5fc47bfcf191e2083345843c
Vagrant Default User <vagrant@jessie.raw>
2017-06-01 14:52:17 +0000

    synchro

---
+++ exemple/phptemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+
+include(__DIR__.'/../bootstrap.php');
+ini_set('display_errors', 'on');
+
+
+$template=new \Phi\PHPTemplate('<h1>hello world <?php echo $name;?></h1>');
+
+echo $template->render(null, array(
+    'name' => 'John Do'
+));
+
--- source/class/PHPTemplate.php
+++ source/class/PHPTemplate.php
@@ -10,74 +10,77 @@ class PHPTemplate
 {
 
 
-	use Collection;
+    use Collection;
 
 
-	protected $template=null;
-	protected $components=array();
+    protected $template = null;
+    protected $components = array();
 
 
-
-	public function __construct($template=null) {
-		$this->template=$template;
-	}
-
-
-
-	public function set($name, $value) {
-		return $this->setVariable($name, $value);
-	}
-	public function &get($name) {
-		return $this->getVariable($name);
-	}
+    public function __construct($template = null)
+    {
+        $this->template = $template;
+    }
 
 
+    public function set($name, $value)
+    {
+        return $this->setVariable($name, $value);
+    }
 
-	public function render($template=null, $values=null) {
-		if($template!==null) {
-			$this->template=$template;
-		}
-
-		if(is_array($values)) {
-			$this->setVariables($values);
-		}
-
-
+    public function &get($name)
+    {
+        return $this->getVariable($name);
+    }
 
 
+    public function render($template = null, $values = null)
+    {
+        if ($template !== null) {
+            $this->template = $template;
+        }
 
-		if(is_file($this->template) && realpath($this->template)) {
-
-
-			ob_start();
-			extract($this->getVariables());
-			include($template);
-			$buffer=ob_get_clean();
-			return $buffer;
-
-		}
-		else {
-			return '';
-		}
-	}
+        if (is_array($values)) {
+            $this->setVariables($values);
+        }
 
 
-	public function __toString() {
-		$buffer=$this->render();
-		return $buffer;
-	}
+        if (is_file($this->template) && realpath($this->template)) {
 
 
+            ob_start();
+            extract($this->getVariables());
+            include($template);
+            $buffer = ob_get_clean();
+            return $buffer;
 
-	public function __set($name, $value) {
-		return $this->set($name, $value);
-	}
+        } else {
+            ob_start();
+            extract($this->getVariables());
+            eval('?>'.$this->template);
+            $buffer = ob_get_clean();
+            return $buffer;
+        }
+    }
 
 
-	public function &__get($name) {
-		return $this->get($name);
-	}
+    public function __toString()
+    {
+        $buffer = $this->render();
+        return $buffer;
+    }
 
+
+    public function __set($name, $value)
+    {
+        return $this->set($name, $value);
+    }
+
+
+    public function &__get($name)
+    {
+        return $this->get($name);
+    }
 
 
 }
