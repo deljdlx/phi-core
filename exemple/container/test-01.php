a71bc1d9be27aed41f352e5f2a473d43bf60d830
Julien Delsescaux <julien.delsescaux@habitatpresto.com>
2017-07-04 16:24:50 +0200

    synchro

---
+++ exemple/container/test-01.php
@@ -0,0 +1,38 @@
+<?php
+
+ini_set('display_errors', 'on');
+require(__DIR__.'/../../bootstrap.php');
+
+
+
+class Test
+{
+    use \Phi\Container\Traits\Depends;
+
+    protected $dependencies=array(
+        'test'=>null
+    );
+}
+
+class Injected
+{
+    public function hello() {
+        return 'hello world';
+    }
+}
+
+
+$container=new \Phi\Container\Container();
+$container->set('test', function() {
+    return new Injected();
+});
+
+
+
+$test=new Test();
+$test->resolveDependencies($container);
+
+echo '<pre id="' . __FILE__ . '-' . __LINE__ . '" style="border: solid 1px rgb(255,0,0); background-color:rgb(255,255,255)">';
+echo '<div style="background-color:rgba(100,100,100,1); color: rgba(255,255,255,1)">' . __FILE__ . '@' . __LINE__ . '</div>';
+print_r($test);
+echo '</pre>';
