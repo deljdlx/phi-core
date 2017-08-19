dde5db835dfb30eb8697d1bbc263483b5579774e
Jean luc <you@example.com>
2017-08-19 18:55:22 +0200

    synchro

--- source/helper/phi.php
+++ source/helper/phi.php
@@ -1,5 +1,63 @@
 <?php
 
+
+function slugify($string)
+{
+
+    $normalized = $string;
+    $normalized = removeAccent($normalized);
+    $normalized=preg_replace('`\W`', '-', $normalized);
+    $normalized=preg_replace('`-+`', '-', $normalized);
+    return $normalized;
+}
+
+
+function normalizeFilepath($filepath)
+{
+    return str_replace('\\', '/', (string)$filepath);
+}
+
+function filepathToClassName($string)
+{
+
+    $string = strtolower(str_replace(
+        '.php',
+        '',
+        str_replace('/', '\\', $string)
+    ));
+
+
+    $string = str_replace('\class\\', '\\', $string);
+
+    return $string;
+}
+
+function removeAccent($str, $charset = 'utf-8')
+{
+    $str = htmlentities($str, ENT_NOQUOTES, $charset);
+    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+    $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+    return $str;
+}
+
+
+
+
+function obinclude($file, $variables = array())
+{
+    ob_start();
+    extract($variables);
+    include($file);
+    return ob_get_clean();
+}
+
+
+
+
+
+
+
 function rglob($pattern, $flags = 0)
 {
     $files = glob($pattern, $flags);
