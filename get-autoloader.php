3f513386049d2d65f067712a0eea149443a83af4
Jean luc <you@example.com>
2019-02-07 18:07:28 +0100

    synchro

--- get-autoloader.php
+++ get-autoloader.php
@@ -7,20 +7,18 @@ if(!defined('PHI_CORE_INITIALIZED')) {
     define('PHI_CORE_INITIALIZED', true);
     require(__DIR__.'/source/class/Autoloader.php');
 
-    $autoloader=new \Phi\Core\Autoloader();
-    $autoloader->addNamespace('Phi\Core', __DIR__.'/source/class');
-    $autoloader->register();
+    $PhiAutoloader=new \Phi\Core\Autoloader();
+    $PhiAutoloader->addNamespace('Phi\Core', __DIR__.'/source/class');
+    $PhiAutoloader->register();
 
-
-    //function getPath
-
-
-
-    return $autoloader;
+    return $PhiAutoloader;
 
 }
 else {
-    throw new \Phi\Core\Exception('Phi bootstrap called twice');
+
+    return $PhiAutoloader;
+
+    //throw new \Phi\Core\Exception('Phi bootstrap called twice');
 }
 
 
